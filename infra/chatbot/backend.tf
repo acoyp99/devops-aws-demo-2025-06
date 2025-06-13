9438ff55cbcc79c0f76ca3faab6261bc6475f2c8
acoyp99 <acoyp1999@gmail.com>
2025-06-12 19:12:17 -0500

    Add gitignore

--- infra/chatbot/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "devops-tf-state-bucket"
-    key            = "chatbot-poc/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-  }
-}
--- infra/chatbot/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-key_name     = "chatbot-keypair"
-db_user      = "chatbotadmin"
-db_pass      = "StrongPassword123!"
-domain_name  = "devops-chatbot.com"
